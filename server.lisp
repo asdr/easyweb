b6f037fe24ba868c5775f44e0010b2f87c71eb56
asdr <asdr@vdeb.(none)>
2011-01-31 20:40:54 +0200

    application-starter%%

--- server.lisp
+++ server.lisp
@@ -1,8 +1,18 @@
 (in-package :easyweb)
 
-(defparameter *listen-address* "localhost")
+(defstruct (easy-starter)
+  "Easy Starter"
+  handle
+  acceptor
+  applications)
 
-(defparameter *listen-port* 8000) 
+(defparameter *acceptor-table* (make-hash-table :test #'equal)
+  "Defined acceptors are kept.")
+
+(defparameter *listen-address* nil
+  "Listens connections from this address, if nil all ip address will be listened.")
+
+(defparameter *listen-port* 8000)
 
 (defparameter *httpd* nil) 
 
@@ -14,9 +24,23 @@
   (setf *httpd* (hunchentoot:start (make-instance 'hunchentoot:acceptor 
 						  :port port
 						  :name "easyweb-default-acceptor"
-						  #|:address address|#))))
+						  :address address))))
 
 (defun server-stop ()
   (hunchentoot:stop *httpd*)
   (setf *httpd* nil))
 
+(defun get-easy-starter (&key (port *listen-port*) (address *listen-address*) name)
+  (let ((key (format nil "~A" (cons address port))))
+    (or (gethash key *acceptor-table*)
+	(setf (gethash key *acceptor-table*)
+	      (make-easy-starter :acceptor (make-instance 'hunchentoot:acceptor 
+							  :address address
+							  :port port
+							  :name name))))))
+
+(defun application-start (application-name &key (port *listen-port*) (address *listen-address*))
+  (when application-name
+    (let ((starter (get-easy-starter :address address
+				     :port port)))
+      )))
