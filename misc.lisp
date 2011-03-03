ce59b5c0cd9f213ef93efeca591af1e239fcebda
Serdar Gökçen <serdargokcen@imac.local>
2011-03-03 23:16:57 +0200

    defview changed

--- misc.lisp
+++ misc.lisp
@@ -1,6 +1,6 @@
 (in-package :easyweb)
 
-(defmacro defview (name inner-args (&rest arguments) &body body)
+#|(defmacro defview (name inner-args (&rest arguments) &body body)
   `(progn 
      (defun ,name (&rest ,inner-args ,@(let ((ret (mapcar #'(lambda(arg)
 							      (when (listp arg)
@@ -14,4 +14,23 @@
        
        ,@body)
      
-     (cl:export ',name cl:*package*)))
+     (cl:export ',name cl:*package*)))|#
+
+(defmacro defview (name &key url-pattern &body body)
+  (if (and (listp url-pattern)
+	   (listp (car body))) ;; the first element of body must include list of arguments
+      `(let ()
+	 (defun ,name (,@(let ((ret (mapcar #'(lambda(arg)
+						(when (listp arg)
+						  (setf (cadr arg)
+							(enclose-string (format nil "~A" (cadr arg)))))
+						arg)
+					   (car body))))
+			      (when ret
+				(push '&key ret))))
+	   ,@(cadr body))
+	 
+	 (cl:export ',name)
+
+	 (define-url-patterns ""
+	     (,url-pattern ',name)))))
